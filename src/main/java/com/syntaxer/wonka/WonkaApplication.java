0f33ef589f0ce052ecc758a3638ba66b21353093
synt4xer <f.syntaxer@gmail.com>
2020-02-04 13:44:12 +0700

    Initial commit

---
+++ src/main/java/com/syntaxer/wonka/WonkaApplication.java
@@ -0,0 +1,14 @@
+package com.syntaxer.wonka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonkaApplication.class, args);
+        //a
+    }
+
+}
---
+++ src/test/java/com/syntaxer/wonka/WonkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxer.wonka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
